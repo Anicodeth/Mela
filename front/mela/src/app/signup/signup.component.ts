2c152c3a076ee4354f1e6c7e91cf1a51d0b85b4f
Leul Abay <leulabay1@gmail.com>
2023-09-11 19:35:16 +0300

    fix signup page

--- front/mela/src/app/signup/signup.component.ts
+++ front/mela/src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {UserService} from "../services/user.service";
+import {RegisterUser} from "../models/registerUser";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  signUpForm: FormGroup;
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.signUpForm = this.fb.group({
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      phoneNumber: ["", [Validators.required]],
+      password: ["", [Validators.required, Validators.minLength(4)]],
+      passwordConfirm: ["", [Validators.required, Validators.minLength(4)]]
+    })
+
+  }
+
+  onSubmit(){
+    console.log("form object:", this.signUpForm.value)
+    this.userService.registerUser(this.signUpForm.value as RegisterUser)
+  }
 
 }
