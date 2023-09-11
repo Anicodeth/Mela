e39f4bd26bf70b1a0d4f3fbecee7ab5ca5ab8823
Leul Abay <leulabay1@gmail.com>
2023-09-11 19:34:04 +0300

    add signup page with full functionalites

--- front/mela/src/app/login/login.component.ts
+++ front/mela/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  signInForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.signInForm = fb.group({
+      email:["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  onSubmit(){
+    this.userService.loginUser(this.signInForm.value)
+  }
 
 }
