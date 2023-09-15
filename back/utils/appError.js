f3fc8cb14c547d23b5055c27b30dd1226468ed8b
Leul Abay <leulabay1@gmail.com>
2023-09-15 07:42:05 +0300

    add the file uploader util

--- back/utils/appError.js
+++ back/utils/appError.js
@@ -1,8 +1,9 @@
 //Creating a custom Error class
 
-const AppError = function(message, statusCode){
+const AppError = function(name, message, statusCode){
 
     Error.call(this,message);
+    this.name = name;
     this.message = message;
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4')?'FAIL':'ERROR';
